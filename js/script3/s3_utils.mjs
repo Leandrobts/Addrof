0e5fa4f03fe342987088cc2958f9258b300917cd
Leandro1986 <kazaalite_601@hotmail.com>
2025-06-04 10:57:09 -0300

    Update s3_utils.mjs

--- js/script3/s3_utils.mjs
+++ js/script3/s3_utils.mjs
@@ -1,6 +1,8 @@
-// js/script3/s3_utils.mjs
+// js/script3/s3_utils.mjs (R31)
+
 import { logToDiv } from '../logger.mjs';
-import { PAUSE as genericPause } from '../utils.mjs';
+// <<<< R31: Importando pauseAsync de utils.mjs >>>>
+import { pauseAsync } from '../utils.mjs'; 
 
 export const SHORT_PAUSE_S3 = 50;
 export const MEDIUM_PAUSE_S3 = 500;
@@ -9,4 +11,5 @@ export const logS3 = (message, type = 'info', funcName = '') => {
     logToDiv('output-advanced', message, type, funcName);
 };
 
-export const PAUSE_S3 = (ms = SHORT_PAUSE_S3) => genericPause(ms);
+// <<<< R31: PAUSE_S3 agora usa pauseAsync >>>>
+export const PAUSE_S3 = (ms = SHORT_PAUSE_S3) => pauseAsync(ms);
