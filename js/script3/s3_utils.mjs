ca719c7842d0373730dd122dda534e8099bb9131
Leandro1986 <kazaalite_601@hotmail.com>
2025-06-04 12:53:08 -0300

    Update s3_utils.mjs

--- js/script3/s3_utils.mjs
+++ js/script3/s3_utils.mjs
@@ -1,8 +1,8 @@
-// js/script3/s3_utils.mjs (R36)
+// js/script3/s3_utils.mjs (R37)
 
 import { logToDiv } from '../logger.mjs';
-// <<<< R36: Importar genericPause_R36 de utils.mjs >>>>
-import { genericPause_R36 } from '../utils.mjs'; 
+// <<<< R37: Importar PAUSE diretamente de utils.mjs >>>>
+import { PAUSE } from '../utils.mjs'; 
 
 export const SHORT_PAUSE_S3 = 50;
 export const MEDIUM_PAUSE_S3 = 500;
@@ -11,5 +11,5 @@ export const logS3 = (message, type = 'info', funcName = '') => {
     logToDiv('output-advanced', message, type, funcName);
 };
 
-// <<<< R36: PAUSE_S3 agora usa a genericPause_R36 importada >>>>
-export const PAUSE_S3 = (ms = SHORT_PAUSE_S3) => genericPause_R36(ms);
+// <<<< R37: PAUSE_S3 agora usa diretamente a função PAUSE importada de utils.mjs >>>>
+export const PAUSE_S3 = (ms = SHORT_PAUSE_S3) => PAUSE(ms);
