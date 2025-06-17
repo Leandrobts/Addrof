708f50e53c6e94215fdefe3b0036170d0e8141cb
Leandro1986 <kazaalite_601@hotmail.com>
2025-06-16 22:13:57 -0300

    Update runAllAdvancedTestsS3.mjs

--- js/script3/runAllAdvancedTestsS3.mjs
+++ js/script3/runAllAdvancedTestsS3.mjs
@@ -1,10 +1,7 @@
-// js/script3/runAllAdvancedTestsS3.mjs (ATUALIZADO para v109)
+// js/script3/runAllAdvancedTestsS3.mjs (ATUALIZADO para v110)
 import { logS3 } from './s3_utils.mjs';
 import { getRunBtnAdvancedS3 } from '../dom_elements.mjs';
-import { 
-    runFinalUnifiedTest, 
-    FNAME_MODULE_FINAL
-} from './testArrayBufferVictimCrash.mjs';
+import { runFinalUnifiedTest, FNAME_MODULE_FINAL } from './testArrayBufferVictimCrash.mjs';
 
 export async function runAllAdvancedTestsS3() {
     const FNAME_ORCHESTRATOR = `${FNAME_MODULE_FINAL}_MainOrchestrator`;
@@ -14,10 +11,10 @@ export async function runAllAdvancedTestsS3() {
     
     logS3(`\n==== Script 3 (${FNAME_ORCHESTRATOR}) CONCLUÍDO ====`, 'test', FNAME_ORCHESTRATOR);
     
-    if(result.success && result.webkit_base) {
+    if(result.success && result.message.includes("ROP: SUCESSO")) {
+        document.title = `${FNAME_MODULE_FINAL}: ROP READY!`;
+    } else if (result.success && result.message.includes("WebKit Base:")) {
         document.title = `${FNAME_MODULE_FINAL}: WebKit Base Leaked!`;
-    } else if (result.success) {
-        document.title = `${FNAME_MODULE_FINAL}: R/W OK, Leak FAILED`;
     } else {
         document.title = `${FNAME_MODULE_FINAL}: FAILED`;
     }
