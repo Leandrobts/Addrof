7503bfaadf9b70af75d9b3826e396c5d67cd6375
Leandro1986 <kazaalite_601@hotmail.com>
2025-06-16 21:48:10 -0300

    Update runAllAdvancedTestsS3.mjs

--- js/script3/runAllAdvancedTestsS3.mjs
+++ js/script3/runAllAdvancedTestsS3.mjs
@@ -1,20 +1,25 @@
-// js/script3/runAllAdvancedTestsS3.mjs (ATUALIZADO para chamar o teste de portabilidade)
+// js/script3/runAllAdvancedTestsS3.mjs (ATUALIZADO para v107)
 import { logS3 } from './s3_utils.mjs';
 import { getRunBtnAdvancedS3 } from '../dom_elements.mjs';
-import { runPortedTechniqueTest, FNAME_MODULE_PORTED_TECH } from './testArrayBufferVictimCrash.mjs';
+// Importa a função principal e o nome do módulo do nosso script portado
+import { 
+    executeTypedArrayVictimAddrofAndWebKitLeak_R43, 
+    FNAME_MODULE_TYPEDARRAY_ADDROF_V82_AGL_R43_WEBKIT 
+} from './testArrayBufferVictimCrash.mjs';
 
 export async function runAllAdvancedTestsS3() {
-    const FNAME_ORCHESTRATOR = `${FNAME_MODULE_PORTED_TECH}_MainOrchestrator`;
+    const FNAME_ORCHESTRATOR = `${FNAME_MODULE_TYPEDARRAY_ADDROF_V82_AGL_R43_WEBKIT}_MainOrchestrator`;
     logS3(`==== INICIANDO Script 3 (${FNAME_ORCHESTRATOR}) ... ====`, 'test', FNAME_ORCHESTRATOR);
     
-    const result = await runPortedTechniqueTest();
+    // Chama a função principal do arquivo testArrayBufferVictimCrash.mjs
+    const result = await executeTypedArrayVictimAddrofAndWebKitLeak_R43();
     
     logS3(`\n==== Script 3 (${FNAME_ORCHESTRATOR}) CONCLUÍDO ====`, 'test', FNAME_ORCHESTRATOR);
     
     if(result.success) {
-        document.title = `${FNAME_MODULE_PORTED_TECH}: PORT SUCCESS!`;
+        document.title = `${FNAME_MODULE_TYPEDARRAY_ADDROF_V82_AGL_R43_WEBKIT}: SUCCESS!`;
     } else {
-        document.title = `${FNAME_MODULE_PORTED_TECH}: Port Failed`;
+        document.title = `${FNAME_MODULE_TYPEDARRAY_ADDROF_V82_AGL_R43_WEBKIT}: FAILED`;
     }
     
     logS3(`Resultado final: ${result.message}`, result.success ? "good" : "error", FNAME_ORCHESTRATOR);
