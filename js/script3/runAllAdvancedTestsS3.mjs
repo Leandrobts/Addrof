d8ae5bc78bccc3123b9b3fcd8837a19edd4f5ac9
Leandro1986 <kazaalite_601@hotmail.com>
2025-06-16 21:51:52 -0300

    Update runAllAdvancedTestsS3.mjs

--- js/script3/runAllAdvancedTestsS3.mjs
+++ js/script3/runAllAdvancedTestsS3.mjs
@@ -1,25 +1,25 @@
-// js/script3/runAllAdvancedTestsS3.mjs (ATUALIZADO para v107)
+// js/script3/runAllAdvancedTestsS3.mjs (ATUALIZADO para v108)
 import { logS3 } from './s3_utils.mjs';
 import { getRunBtnAdvancedS3 } from '../dom_elements.mjs';
-// Importa a função principal e o nome do módulo do nosso script portado
+// Importa a nova função unificada e o nome do módulo
 import { 
-    executeTypedArrayVictimAddrofAndWebKitLeak_R43, 
-    FNAME_MODULE_TYPEDARRAY_ADDROF_V82_AGL_R43_WEBKIT 
+    runFinalUnifiedTest, 
+    FNAME_MODULE_FINAL
 } from './testArrayBufferVictimCrash.mjs';
 
 export async function runAllAdvancedTestsS3() {
-    const FNAME_ORCHESTRATOR = `${FNAME_MODULE_TYPEDARRAY_ADDROF_V82_AGL_R43_WEBKIT}_MainOrchestrator`;
+    const FNAME_ORCHESTRATOR = `${FNAME_MODULE_FINAL}_MainOrchestrator`;
     logS3(`==== INICIANDO Script 3 (${FNAME_ORCHESTRATOR}) ... ====`, 'test', FNAME_ORCHESTRATOR);
     
-    // Chama a função principal do arquivo testArrayBufferVictimCrash.mjs
-    const result = await executeTypedArrayVictimAddrofAndWebKitLeak_R43();
+    // Chama a única função de teste necessária
+    const result = await runFinalUnifiedTest();
     
     logS3(`\n==== Script 3 (${FNAME_ORCHESTRATOR}) CONCLUÍDO ====`, 'test', FNAME_ORCHESTRATOR);
     
     if(result.success) {
-        document.title = `${FNAME_MODULE_TYPEDARRAY_ADDROF_V82_AGL_R43_WEBKIT}: SUCCESS!`;
+        document.title = `${FNAME_MODULE_FINAL}: SUCCESS!`;
     } else {
-        document.title = `${FNAME_MODULE_TYPEDARRAY_ADDROF_V82_AGL_R43_WEBKIT}: FAILED`;
+        document.title = `${FNAME_MODULE_FINAL}: FAILED`;
     }
     
     logS3(`Resultado final: ${result.message}`, result.success ? "good" : "error", FNAME_ORCHESTRATOR);
