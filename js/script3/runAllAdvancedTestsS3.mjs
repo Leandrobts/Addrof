a7c6e8d6eb813d9b635799c727849186adb4b073
Leandro1986 <kazaalite_601@hotmail.com>
2025-06-16 21:38:50 -0300

    Update runAllAdvancedTestsS3.mjs

--- js/script3/runAllAdvancedTestsS3.mjs
+++ js/script3/runAllAdvancedTestsS3.mjs
@@ -1,20 +1,20 @@
-// js/script3/runAllAdvancedTestsS3.mjs (ATUALIZADO para chamar o Teste de Força Total)
+// js/script3/runAllAdvancedTestsS3.mjs (ATUALIZADO para chamar o teste de portabilidade)
 import { logS3 } from './s3_utils.mjs';
 import { getRunBtnAdvancedS3 } from '../dom_elements.mjs';
-import { runFullControlTest, FNAME_MODULE_FULL_CONTROL } from './testArrayBufferVictimCrash.mjs';
+import { runPortedTechniqueTest, FNAME_MODULE_PORTED_TECH } from './testArrayBufferVictimCrash.mjs';
 
 export async function runAllAdvancedTestsS3() {
-    const FNAME_ORCHESTRATOR = `${FNAME_MODULE_FULL_CONTROL}_MainOrchestrator`;
+    const FNAME_ORCHESTRATOR = `${FNAME_MODULE_PORTED_TECH}_MainOrchestrator`;
     logS3(`==== INICIANDO Script 3 (${FNAME_ORCHESTRATOR}) ... ====`, 'test', FNAME_ORCHESTRATOR);
     
-    const result = await runFullControlTest();
+    const result = await runPortedTechniqueTest();
     
     logS3(`\n==== Script 3 (${FNAME_ORCHESTRATOR}) CONCLUÍDO ====`, 'test', FNAME_ORCHESTRATOR);
     
     if(result.success) {
-        document.title = `${FNAME_MODULE_FULL_CONTROL}: ABSOLUTE CONTROL!`;
+        document.title = `${FNAME_MODULE_PORTED_TECH}: PORT SUCCESS!`;
     } else {
-        document.title = `${FNAME_MODULE_FULL_CONTROL}: Control Failed`;
+        document.title = `${FNAME_MODULE_PORTED_TECH}: Port Failed`;
     }
     
     logS3(`Resultado final: ${result.message}`, result.success ? "good" : "error", FNAME_ORCHESTRATOR);
