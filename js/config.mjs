10b5e5783e0a96601199ab27539d9a09aa25d7b4
Leandro1986 <kazaalite_601@hotmail.com>
2025-06-24 22:27:49 -0300

    Update config.mjs

--- js/config.mjs
+++ js/config.mjs
@@ -147,7 +147,7 @@ export const WEBKIT_LIBRARY_INFO = {
 };
 
 export let OOB_CONFIG = {
-    ALLOCATION_SIZE: 0x1048576, // Reduzido para 32KB para maior estabilidade
+    ALLOCATION_SIZE: 0x8000, // Reduzido para 32KB para maior estabilidade
     BASE_OFFSET_IN_DV: 128,
     INITIAL_BUFFER_SIZE: 32
 };
